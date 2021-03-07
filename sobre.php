f75f08b9bab83f972b72610e6f24a6a518d5d367
Waislan Sanches <waislanluis@gmail.com>
2021-03-07 20:03:04 -0300

    Sobre

--- sobre.php
+++ sobre.php
@@ -22,12 +22,31 @@
                 <h2>Sobre nós</h2>
             </header>
             <div class="card-body p-3">
-                <img src="https://placehold.it/200x200" class="img-thumbnail float-left mr-3">
+                <img src="img/fernando.png" style="width: 200px; height: 200px;" class="img-thumbnail float-left mr-3">
                 <p>
-                    <strong>Nome:</strong> Nome do Aluno <br>
-                    <strong>E-mail:</strong> aluno@ifsuldeminas.edu.br<br>
-                    <strong>Curso:</strong> Nome do Curso <br>
-                    <strong>Período:</strong> Período do Aluno
+                    <strong>Nome:</strong> Fernando de Souza Vasconcelos <br>
+                    <strong>E-mail:</strong> fernando.vasconcelos@alunos.ifsuldeminas.edu.br<br>
+                    <strong>Curso:</strong> Engenharia de Computação <br>
+                    <strong>Período:</strong> 8
+                </p>
+                <strong>MINICURRÍCULO:</strong>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis error fugiat nemo veritatis id qui ea repellat quia magni similique cumque voluptates quo perferendis, nulla illo obcaecati ut fuga quibusdam.</p>
+                <p>Odio repudiandae ab explicabo provident! Ullam ea repudiandae nam a commodi error, fuga non earum magnam saepe nesciunt excepturi, temporibus repellendus minus! Rem iste asperiores, dicta facere nisi temporibus esse!</p>
+                <p>Est molestiae eveniet minus sunt dolorum obcaecati harum porro delectus voluptate. Consectetur officia minima nemo vel, itaque ipsam laboriosam! Atque, suscipit. Blanditiis, qui corrupti! Eos minus cum amet officiis rerum.</p>
+            </div>
+        </section>
+
+        <section class="card">
+            <header class="card-header p-3">
+                <h2>Sobre nós</h2>
+            </header>
+            <div class="card-body p-3">
+                <img src="img/waislan.png" style="width: 200px; height: 200px;" class="img-thumbnail float-left mr-3">
+                <p>
+                    <strong>Nome:</strong> Waislan Luis Sanches <br>
+                    <strong>E-mail:</strong> waislan.sanches@alunos.ifsuldeminas.edu.br<br>
+                    <strong>Curso:</strong> Engenharia de Computação <br>
+                    <strong>Período:</strong> 8
                 </p>
                 <strong>MINICURRÍCULO:</strong>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis error fugiat nemo veritatis id qui ea repellat quia magni similique cumque voluptates quo perferendis, nulla illo obcaecati ut fuga quibusdam.</p>
