1ff125628d71e4aaa36e8d0a90cde7066ed2868a
Default37 <fernando_souza_vasconcelos@hotmail.com>
2021-03-17 14:12:31 -0300

    Alteracao - 17/03

--- services/cadastro.php
+++ services/cadastro.php
@@ -12,18 +12,47 @@ $foto = $_POST['foto'];
 $cor = $_POST['cor'];
 $descricao = $_POST['descricao'];
 
-$query = 'INSERT INTO veiculos (marca, modelo, ano, preco, foto, cor, descricao) 
+$_SESSION['valid'] = true;
+valida_campos($marca, $modelo, $ano, $preco, $foto, $cor, $descricao);
+
+if($_SESSION['valid'] == true) {
+    $query = 'INSERT INTO veiculos (marca, modelo, ano, preco, foto, cor, descricao) 
         VALUES (:marca, :modelo, :ano, :preco, :foto, :cor, :descricao)';
 
-try {
-    $statement = $db->prepare($query);
-    $statement->execute($data);
-    $_SESSION['success'] = true;
+    try {
+        $statement = $db->prepare($query);
+        $statement->execute($data);
+        $_SESSION['success'] = true;
 
-} catch (Exception $e) {
-    echo 'Exception -> ' . $e->getMessage();
-    //var_dump($e->getMessage());
-    $_SESSION['success'] = false;
+    } catch (Exception $e) {
+        echo 'Exception -> ' . $e->getMessage();
+        //var_dump($e->getMessage());
+        $_SESSION['success'] = false;
+    }
+    $db = null;
 }
-$db = null;
 header('Location: ../cadastro.php');
+
+function valida_campos($marca, $modelo, $ano, $preco, $foto, $cor, $descricao) {
+    if(isset($marca) != 1) {
+        $_SESSION['valid'] = false;
+    }
+    else if(is_string($modelo) != 1) {
+        $_SESSION['valid'] = false;
+    }
+    else if(($ano < 1960) or ($ano > 2022)) {
+        $_SESSION['valid'] = false;
+    }
+    else if($preco < 1000){
+        $_SESSION['valid'] = false;
+    }
+    else if(isset($foto) != 1) {
+        $_SESSION['valid'] = false;
+    }
+    else if(isset($cor) != 1) {
+        $_SESSION['valid'] = false;
+    }
+    else if(isset($descricao) != 1) {
+        $_SESSION['valid'] = false;
+    }
+}
